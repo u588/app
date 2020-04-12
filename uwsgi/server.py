687c64cd9c9b1d25dd76310043d608e05fd5e257
u588 <u588@sina.com>
2020-04-12 16:12:45 +0800

    ok

--- uwsgi/server.py
+++ uwsgi/server.py
@@ -2,18 +2,18 @@ from flask import Flask, escape, request, redirect
 
 app = Flask(__name__,
                     static_url_path='/',
-                    static_folder='/home/ts/app/www/html',
+                    static_folder='/home/ts/app',
                     template_folder='/home/ts/app/www/templates')
 
 @app.route('/')
 def index():
-    return redirect('000001上证指数.html')
+    return redirect('/www/html/000001上证指数.html')
     # pass
 # app.add_url_rule('/','index', index)
 
-@app.route('/Stocks')
+@app.route('/Stocks/')
 def Stocks():
-    return redirect('0226.txt')
+    return redirect('/data/FindStocks/0101.txt')
 
 if __name__ == '__main__':
     app.run()
