ee47604edfaaca8ca94a9d98fcae49153deac741
u588 <u588@sina.com>
2020-05-09 11:24:03 +0800

    ok

--- www/webapp.py
+++
@@ -1,4 +0,0 @@
-# Entry point for the application.
-# import app    # For application discovery by the 'flask' command.
-from test_app import views   # For import side-effects of setting up routes.S
-
