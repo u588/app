348ca34e96e17bb41051120f894043122d0d644c
u588 <u588@sina.com>
2020-06-08 14:48:20 +0000

    ok

--- getStocksList.py
+++ getStocksList.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 data = ts.get_stock_basics().round(2)
 
-data.to_csv('/home/ts/app/data/StocksList.csv')
+if len(data)>3800:
+    data.to_csv('/home/ts/app/data/StocksList.csv')
+else:
+    pass
