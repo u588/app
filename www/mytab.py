16cf057a00c3cf82e4d7b3f82485387172ff18df
u588 <u588@sina.com>
2020-07-13 03:08:00 +0000

    u588

--- www/mytab.py
+++ www/mytab.py
@@ -136,7 +136,7 @@ def timeLine_wordCloud(d):
                 .add(series_name="热点分析", data_pair=[list(z) for z in zip(data.name,data.pct_chg)], word_size_range=[6, 66])
                 .set_global_opts(
                     title_opts=opts.TitleOpts(
-                        title="热点分析", title_textstyle_opts=opts.TextStyleOpts(font_size=23)
+                        title="热点分析 "+i, title_textstyle_opts=opts.TextStyleOpts(font_size=23)
                     ),
                     tooltip_opts=opts.TooltipOpts(is_show=True),
                 )
