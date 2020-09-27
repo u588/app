16d40206a55724b7fa3d3b159f125c46f7500023
u588 <u588@sina.com>
2020-09-27 07:41:53 +0000

    ok

---
+++ pyScripts/SQL/cicsClass.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/StockBas')
+
+df = pd.read_excel('/home/ts/app/data/cicsClass.xls')
+df.exchange.replace('Shenzhen', 'sz', inplace=True)
+df.exchange.replace('Shanghai', 'sh', inplace=True)
+df.set_index('ts_code').to_sql('cicsClass', eng , if_exists='replace')
