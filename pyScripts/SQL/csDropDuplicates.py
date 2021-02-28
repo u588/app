055280c7870aadafef12171e55240a2f25852e84
u588 <u588@sina.com>
2021-02-28 21:40:11 +0800

    ok

--- pyScripts/SQL/csDropDuplicates.py
+++ pyScripts/SQL/csDropDuplicates.py
@@ -1,17 +1,18 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56:5432/csIndex')
-IndexLists = pd.read_sql('csIndexs', eng).Index_code.to_list()
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56:5432/Funds')
+engs = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56:5432/tdxStocks')
+StockLists = pd.read_sql('StocksList', engs).code.tolist()
 
-for i, CodeID in enumerate(IndexLists):
-    print('Index', i, '/', len(IndexLists))
+for i, CodeID in enumerate(StockLists):
+    print('Index', i, '/', len(StockLists))
     # if StockCode >= '603885':
     try:
         df = pd.read_sql(CodeID, eng)
-        df.drop_duplicates(subset='Date', keep='first', inplace=True)
+        df.drop_duplicates(subset='date', keep='first', inplace=True)
 
-        df.set_index('Date', inplace=True)
+        df.set_index('date', inplace=True)
         df.to_sql(CodeID, eng, if_exists='replace')
         print(CodeID,'saved to sql !')
     except:
