58ed0c8aa7e9d764834706af64659001816cc36a
u588 <u588@sina.com>
2023-05-03 19:10:06 +0800

    ok

---
+++ HugeGraph/test/test.py
@@ -0,0 +1,16 @@
+from hugegraph import PyHugeGraph
+# init client
+client = PyHugeGraph("127.0.0.1", "8080", user="admin", pwd="pwd", graph="hugegraph")
+
+# schema
+schema = client.schema()
+schema.getVertexLabels()
+
+# graph
+g = client.graph()
+g.getVertexById("1:tom")
+g.close()
+
+# gremlin
+gremlin = client.gremlin()
+gremlin.exec("g.V().limit(10)")
