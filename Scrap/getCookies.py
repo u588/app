edd605ab4ab338f58b1e8e1ebfa5eecb45dcab47
u588 <u588@sina.com>
2023-05-19 08:51:20 +0800

    ok

--- Scrap/getCookies.py
+++ Scrap/getCookies.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 web = webdriver.Remote(command_executor='http://10.3.18.65:11111/wd/hub',desired_capabilities=DesiredCapabilities.CHROME)
 web.get('https://www.csindex.com.cn/#/')
 
+
+
 submit_btn = web.find_element_by_id('nc_1_n1z')
 
 ActionChains(web).drag_and_drop_by_offset(submit_btn,xoffset = 300,yoffset = 0).perform()
