476eea808651f5cea1a7331125a63dea9899d89b
u588 <u588@sina.com>
2023-06-07 16:13:17 +0800

    1

--- DP_code/www/detailChart.py
+++ DP_code/www/detailChart.py
@@ -1,9 +1,7 @@
-import re
 from pyecharts.charts.composite_charts.grid import Grid
 from pyecharts.options.global_options import InitOpts
 from sqlalchemy import create_engine
 import pandas as pd
-import numpy as np
 from pyecharts.components import Table
 from pyecharts.charts import Pie, Line
 from pyecharts import options as opts
@@ -33,51 +31,14 @@ def line(StockID) -> Line:
     c = (
         Line(opts.InitOpts(page_title='财务分析',width="1330px", height="600px"))
         .add_xaxis(date)
-        .set_global_opts(legend_opts=opts.LegendOpts(type_='scroll',orient='vertical',pos_right='-1%',pos_top='10%',is_show=True),title_opts=opts.TitleOpts(title=StockID, pos_left="center",pos_top="5"),)
+        .set_global_opts(xaxis_opts=opts.AxisOpts(type_="category", boundary_gap=False),legend_opts=opts.LegendOpts(orient='vertical',pos_right='0%',pos_top='10%',is_show=True),title_opts=opts.TitleOpts(title=StockID, pos_left="center",pos_top="5"),)
     )
     for i, n in enumerate(d.tolist()[1:]):
         c.add_yaxis(d.tolist()[1:][i], d1s[d1s.columns[i+1]].tolist(),is_smooth=True, label_opts=opts.LabelOpts(is_show=False),is_selected=False)
-    return c
+ 
+    grid_chart = Grid(opts.InitOpts(page_title='分析', width="1350px", height="600px"))
 
+    grid_chart.add(c, grid_opts=opts.GridOpts(border_width=0, pos_left="5%", pos_right="12%"),)
 
-def pie(StockID):
-
-    StocksList = pd.read_csv('/home/ts/app/data/StocksList.csv', dtype={'code':object})
-    Stock = StocksList.loc[StocksList['code']==StockID].astype(str).reset_index()
-
-    IndexConst = pd.read_sql('csIndexCons', eng)
-    StockInIndex = IndexConst[IndexConst.code==StockID][['Index_code', 'code','name']]
-    StockInIndex.rename(columns={'name':'stock_name','code':'stock_code'}, inplace=True)
-    csIndex = pd.read_sql('csIndexs', eng)
-    csIndex =csIndex[['Index_code', 'Index_name']]
-
-    data = pd.merge(StockInIndex, csIndex, on='Index_code')  
-
-    dd = data[['Index_name', 'Index_code']]
-    d = data.stock_name[0]+" : " + data.stock_code[0]
-    c = (
-        Pie()
-        .add(
-            "",
-            [list(z) for z in zip(dd.Index_name, dd.Index_code)],
-            radius=["25%", "70%"],
-            center=["50%", "50%"],
-            rosetype="area",
-        )
-        .set_global_opts(
-                        title_opts=opts.TitleOpts(
-                                    title=(d),pos_left="center",pos_top="20",
-                          
-                        #             subtitle = ('所属行业: '+Stock['industry'][0]+'  地域: '+Stock['area'][0]+'  市盈率: '+Stock['pe'][0]+'  总股本: '+Stock['totals'][0]+'亿元'+
-                        #     '  流通股本: '+Stock['outstanding'][0]+'亿元'+'  市净率:'+Stock['pb'][0]+'  每股收益:'+Stock['esp'][0]+ '  每股净资:'+Stock['bvps'][0]+
-                        #     '  每股分配利润:'+Stock['perundp'][0]+'  收入同比:'+Stock['rev'][0]+'%'+'  利润同比:'+Stock['profit'][0]+'%'+'  毛利率:'+Stock['gpr'][0]+'%'+
-                        #     '  净利润率:'+Stock['npr'][0]+'%'), 
-                        #             subtitle_textstyle_opts=opts.TextStyleOpts(color='blue',font_style='italic',font_weight='bold'),
-                        ),
-                        legend_opts=opts.LegendOpts(is_show=False),
-        )
-        .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
-    )
-    return c
-
+    return grid_chart
 
