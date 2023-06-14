d3e4fd8ca0a6a9622900bf50b83981b485c3a110
u588 <u588@sina.com>
2023-06-14 16:58:31 +0800

    ok

---
+++ Ding/test.py
@@ -0,0 +1,42 @@
+import uiautomator2 as u2
+import sys
+from chinese_calendar import is_workday, is_holiday
+import time
+import datetime
+import random
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+d = u2.connect('10.3.68.199')
+
+d.app_start('com.tdx.AndroidNew', stop=True)
+
+d.xpath('//*[@content-desc="今日有 3 只新股可申购"]/android.widget.ImageView[3]').click()
+d(description="股票/功能/资讯").click()
+d(focused=True).set_text('881001')
+d(description="确定").click()
+d(scrollable=True).scroll.toEnd()
+
+d.xpath('//android.widget.ScrollView/android.widget.LinearLayout[1]/android.widget.LinearLayout[3] \
+        /android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2] \
+        /android.view.View[1]').info['bounds']['bottom']
+d.click(10, 1600)
+
+
+d.click(786, 2172)
+
+d.xpath('//android.widget.HorizontalScrollView').info['childCount']
+
+d.xpath('//android.widget.HorizontalScrollView/android.view.View[48]/android.view.View').info['contentDescription'] 
+d.xpath('//android.widget.HorizontalScrollView/android.view.View[1]/android.view.View').info
+
+
+for elem in d.xpath("//android.widget.HorizontalScrollView").all():
+    print("Text:", elem.text)
+    # Dictionary eg: 
+    # {'index': '1', 'text': '999+', 'resource-id': 'com.netease.cloudmusic:id/qb', 'package': 'com.netease.cloudmusic', 'content-desc': '', 'checkable': 'false', 'checked': 'false', 'clickable': 'false', 'enabled': 'true', 'focusable': 'false', 'focused': 'false','scrollable': 'false', 'long-clickable': 'false', 'password': 'false', 'selected': 'false', 'visible-to-user': 'true', 'bounds': '[661,1444][718,1478]'}
+    print("Attrib:", elem.attrib)
+    # Coordinate eg: (100, 200)
+    print("Position:", elem.center())
