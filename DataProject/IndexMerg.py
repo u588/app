407cfb2b41018c8d5745c123cf40ca4ba224ed98
u588 <u588@sina.com>
2023-06-15 23:30:05 +0800

    0

--- DataProject/IndexMerg.py
+++ DataProject/IndexMerg.py
@@ -5,8 +5,12 @@ tdxzz = pd.read_excel('G:/Gitee/App/Data/TDXdata/New/zzIndexs.xlsx', dtype={'Ind
 tdxsh = pd.read_excel('G:/Gitee/App/Data/TDXdata/New/shIndexs.xlsx', dtype={'IndexCode':object})
 tdxsz = pd.read_excel('G:/Gitee/App/Data/TDXdata/New/szIndexs.xlsx', dtype={'IndexCode':object})
 cs = pd.read_excel('G:/Gitee/App/Data/CSdata/csIndex2023.xlsx', dtype={'IndexCode':object})
-tdx = pd.read_excel('G:/Gitee/App/Data/TDXdata/New/tdxtext.xlsx', dtype={'IndexCode':object})
+tdx = pd.read_excel('G:/Gitee/App/Data/TDXdata/New/IndexsMergeRaw2023.xlsx', dtype={'IndexCode':object})
 
 tdx.drop_duplicates(subset='IndexCode', keep='first', inplace=True)
 
 m1 = pd.merge(tdx, cs, on='IndexCode', how='outer')
+
+#NaN值处理
+
+tdx['Num'] = tdx.Num_x.fillna(tdx.Num_y)
