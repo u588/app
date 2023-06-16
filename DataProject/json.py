ce63c101082f2f1dc475907f0cb55ba0a8cd6ab4
u588 <u588@sina.com>
2023-06-16 17:26:52 +0800

    1

--- DataProject/json.py
+++ DataProject/json.py
@@ -4,13 +4,14 @@ import pandas as pd
 a = open('G:/Gitee/App/1.json', 'r').read()
 data = json.loads(a)
 n = len(data)
-q = pd.DataFrame
+q = pd.DataFrame(['a','b','b']).T
+
 i=0
 while i < n:
     try:
         aa = data[i]['_source']['layers']['data']['data.data'].replace(':', '')
         aaa = bytes.fromhex(aa).decode(errors='ignore')
-        c = re.findall("\d{6}", aaa)
+        c = re.findall("\d{6}", aaa)[::-1]
         c1 = pd.DataFrame(c).T
         q = pd.concat([q,c1])
         i = i+1
