f64c3dc307b2cefd3d2e6947a68508b43858089d
u588 <u588@sina.com>
2023-06-16 17:16:08 +0800

    1

--- DataProject/json.py
+++ DataProject/json.py
@@ -1,9 +1,22 @@
 import json
 import re
-
+import pandas as pd
 a = open('G:/Gitee/App/1.json', 'r').read()
 data = json.loads(a)
-aa = data[19]['_source']['layers']['data']['data.data'].replace(':', '')
+n = len(data)
+q = pd.DataFrame
+i=0
+while i < n:
+    try:
+        aa = data[i]['_source']['layers']['data']['data.data'].replace(':', '')
+        aaa = bytes.fromhex(aa).decode(errors='ignore')
+        c = re.findall("\d{6}", aaa)
+        c1 = pd.DataFrame(c).T
+        q = pd.concat([q,c1])
+        i = i+1
+        print(str(i) +' concated !')
+    except:
+        i = i + 1
+        pass
 
-aaa = bytes.fromhex(aa).decode(errors='ignore')
-re.findall("\d{6}", aaa)
+
