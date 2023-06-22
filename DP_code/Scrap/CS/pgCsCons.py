bdfc0ea704823110c594b3c4d3477ae8dab507a2
u588 <u588@sina.com>
2023-06-22 22:53:44 +0800

    00

--- DP_code/Scrap/CS/pgCsCons.py
+++ DP_code/Scrap/CS/pgCsCons.py
@@ -8,7 +8,7 @@ import time
 
 #header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',}
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.67',}
-eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56:5432/csIndex')
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56:5432/tdxIndex')
 
 sql = 'DROP TABLE IF EXISTS "csIndexCon";'
 eng.execute(sql)
@@ -17,17 +17,19 @@ time.sleep(5)
 def getData(codeID):
     
     # url = "http://www.csindex.com.cn/zh-CN/indices/index-detail/"+codeID
-    url = "https://csi-web-dev.oss-cn-shanghai-finance-1-pub.aliyuncs.com/static/html/csindex/public/uploads/file/autofile/closeweight/"+codeID+"closeweight.xls"
+    # url = "https://csi-web-dev.oss-cn-shanghai-finance-1-pub.aliyuncs.com/static/html/csindex/public/uploads/file/autofile/closeweight/"+codeID+"closeweight.xls"
+    url = "https://csi-web-dev.oss-cn-shanghai-finance-1-pub.aliyuncs.com/static/html/csindex/public/uploads/file/autofile/cons/"+codeID+"cons.xls"
     r = requests.get(url, headers=header)
     # html= etree.HTML(r.content)
     # urlD = html.xpath("//ul[@class='download clearfix mb-10']//li/a[contains(text(),'成份列表')]")[0].xpath("@href")[0]
     # r = requests.get(urlD, headers=header)
-    a = pd.read_excel(r.content,index_col=None, header=None,skiprows=1,dtype={1:object,4:object})[[1,2,4,5,9]]
-    a.columns=['Index_code', 'Index_name','code','name', 'weight']
-    a.set_index('Index_code',inplace=True)
+    a = pd.read_excel(r.content,index_col=None, header=None,skiprows=1,dtype={1:object,4:object})[[1,2,4,5]]
+    a.columns=['IndexCode', 'IndexName','StockCode','StockName']
+    a.set_index('IndexCode',inplace=True)
     return a
 
-IndexLists = pd.read_sql('csIndexs', eng).Index_code.to_list()
+df = pd.read_sql('tdxIndexs', eng)
+IndexLists = df.loc[df.From=='CS'].IndexCode.tolist()
 
 for codeID in IndexLists:
     try:
