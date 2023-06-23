5676dda3974d4dd70b4cf7dbd923e71d144ab04d
u588 <u588@sina.com>
2023-06-23 09:28:38 +0800

    q

--- DataProject/tdxGui_BLK_CSConcat.py
+++ DataProject/tdxGui_BLK_CSConcat.py
@@ -1,8 +1,11 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
 
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxIndex')
+
+CsCons = pd.read_sql('csIndexCon',eng)
 BLKCons = pd.read_excel('G:/Gitee/App/tdxAppData/1tdxIndexsConsBLK.xlsx', dtype={'IndexCode':object, 'StockCode':object})
-CsCons = pd.read_excel('G:/Gitee/App/tdxAppData/1csIndexCons.xlsx', dtype={'IndexCode':object, 'StockCode':object})
 GUICons = pd.read_excel('G:/Gitee/App/tdxAppData/1tdxGuiIndexCons.xlsx', dtype={'IndexCode':object, 'StockCode':object})
 
 df = pd.DataFrame
@@ -10,3 +13,4 @@ df = pd.concat([CsCons, BLKCons])
 df = pd.concat([df,GUICons])
 
 df.drop_duplicates().set_index('IndexCode').to_excel('G:/Gitee/App/tdxAppData/FinaltdxIndexCons622.xlsx')
+df.drop_duplicates().set_index('IndexCode').to_sql('IndexCons', eng, if_exists='replace')
