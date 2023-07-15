6384234a6ff4922e0372c9703ea92a92443b956a
u588 <u588@sina.com>
2023-07-15 17:29:54 +0800

    0

--- MathAn/PlotData.py
+++ MathAn/PlotData.py
@@ -9,9 +9,9 @@ mpl.rcParams['font.sans-serif'] = ['KaiTi']
 
 
 ConsName = '中证水泥'
-df = pd.read_csv('f:/StocksData/Cons/' +ConsName + '.csv', index_col=0, dtype={'st_code':object})
-df['open_round'] = df.open_chg.round(0)
-df['day_round'] = df.day_chg.round(0)
+df = pd.read_excel('G:\Gitee\App\MathAn\ell.xlsx')
+# df['open_round'] = df.open_chg.round(0)
+# df['day_round'] = df.day_chg.round(0)
 
 dd = df[['open_chg', 'day_chg', 'close']].groupby(['day_chg', 'open_chg']).count().reset_index()
 # dd = df[['open_round', 'day_round', 'close']].groupby(['day_round', 'open_round']).count().reset_index()
@@ -23,9 +23,11 @@ pt = dd.pivot(index='open_chg', columns='day_chg', values='close')
 # pt = dd.pivot(index='open_round', columns='day_round', values='close')
 f, ax = plt.subplots(figsize = (16,9))
 
-sns.heatmap(pt, cmap = 'cool',linewidths=0.1, linecolor=(0.1, 0.2, 0.5, 0.05))  
-ax.set_title(ConsName)
-ax.set_xlabel('day_chg')
-ax.set_ylabel('open_chg')
+
+g = sns.scatterplot(x='stb', y='am', hue='eq', style='eq',data= df)
+g.set(xscale="log", yscale="log")
+g.ax.xaxis.grid(True, "minor", linewidth=.25)
+g.ax.yaxis.grid(True, "minor", linewidth=.25)
+g.despine(left=True, bottom=True)
 
 plt.show()
