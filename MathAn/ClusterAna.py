40a05931f169c1dda0b765a6122d78504603f612
u588 <u588@sina.com>
2023-07-15 16:10:19 +0800

    0

---
+++ MathAn/ClusterAna.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.collections import LineCollection
+from sklearn import cluster, covariance, manifold
+from sqlalchemy import create_engine
+
+home = '10.145.254.55:5432'
+job = '10.3.18.56:5432'
+ip = job
+
+eng = create_engine('postgresql+psycopg2://sa:11111111@' + ip + '/tdxStocks')
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+IndexConsts = pd.read_csv('f:\indexconst.csv', dtype={'code':object, 'index_code':object})
+###############################################################################
+# Retrieve the data from Internet
+
+# Choose a time period reasonably calm (not too long ago so that we get
+# high-tech firms, and before the 2008 crash)
+d1 = '2019-04-01'
+d2 = '2019-06-12'
+
+start_date = datetime.strptime(d1,'%Y-%m-%d')
+end_date = datetime.strptime(d2,'%Y-%m-%d')
+
+IndexCode = '399394'
+
+
+Consts = IndexConsts[IndexConsts.index_code==IndexCode]
+symbols = Consts.code.tolist()
+names = Consts.name.tolist() 
+
+quotes = []
+for symbol in symbols:
+    print('Fetching quote history for %r' % symbol)
+    
+    df = pd.read_sql(symbol, eng)[['datetime', 'open', 'close']]
+    df.datetime = df.datetime.str.replace('15:00', '')
+    dd = df[(df['datetime']>d1) & (df['datetime']<d2)]
+    
+    quotes.append(dd)
+
+
+close_prices = np.vstack([q['close'] for q in quotes])
+open_prices = np.vstack([q['open'] for q in quotes])
+
+# 每日价格浮动包含了重要信息！
+variation = close_prices - open_prices
+
+###############################################################################
+# Learn a graphical structure from the correlations
+# GraphLassoCV has been renamed to GraphicalLassoCV
+edge_model = covariance.GraphLassoCV()
+
+# standardize the time series: using correlations rather than covariance
+# is more efficient for structure recovery
+X = variation.copy().T
+X /= X.std(axis=0)
+edge_model.fit(X)
+
+###############################################################################
+# Cluster using affinity propagation
+
+_, labels = cluster.affinity_propagation(edge_model.covariance_)
+n_labels = labels.max()
+
+for i in range(n_labels + 1):
+    print('Cluster %i: %s' % ((i + 1), ', '.join(names[labels == i])))
+
+###############################################################################
+# Find a low-dimension embedding for visualization: find the best position of
+# the nodes (the stocks) on a 2D plane
+
+# We use a dense eigen_solver to achieve reproducibility (arpack is
+# initiated with random vectors that we don't control). In addition, we
+# use a large number of neighbors to capture the large-scale structure.
+node_position_model = manifold.LocallyLinearEmbedding(
+    n_components=2, eigen_solver='dense', n_neighbors=6)
+
+embedding = node_position_model.fit_transform(X.T).T
+
+###############################################################################
+# Visualization
+plt.figure(1, facecolor='w', figsize=(10, 8))
+plt.clf()
+ax = plt.axes([0., 0., 1., 1.])
+plt.axis('off')
+
+# Display a graph of the partial correlations
+partial_correlations = edge_model.precision_.copy()
+d = 1 / np.sqrt(np.diag(partial_correlations))
+partial_correlations *= d
+partial_correlations *= d[:, np.newaxis]
+non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
+
+# Plot the nodes using the coordinates of our embedding
+plt.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
+            cmap=plt.cm.spectral)
+
+# Plot the edges
+start_idx, end_idx = np.where(non_zero)
+#a sequence of (*line0*, *line1*, *line2*), where::
+#            linen = (x0, y0), (x1, y1), ... (xm, ym)
+segments = [[embedding[:, start], embedding[:, stop]]
+            for start, stop in zip(start_idx, end_idx)]
+values = np.abs(partial_correlations[non_zero])
+lc = LineCollection(segments,
+                    zorder=0, cmap=plt.cm.hot_r,
+                    norm=plt.Normalize(0, .7 * values.max()))
+lc.set_array(values)
+lc.set_linewidths(15 * values)
+ax.add_collection(lc)
+
+# Add a label to each node. The challenge here is that we want to
+# position the labels to avoid overlap with other labels
+for index, (name, label, (x, y)) in enumerate(
+        zip(names, labels, embedding.T)):
+
+    dx = x - embedding[0]
+    dx[index] = 1
+    dy = y - embedding[1]
+    dy[index] = 1
+    this_dx = dx[np.argmin(np.abs(dy))]
+    this_dy = dy[np.argmin(np.abs(dx))]
+    if this_dx > 0:
+        horizontalalignment = 'left'
+        x = x + .002
+    else:
+        horizontalalignment = 'right'
+        x = x - .002
+    if this_dy > 0:
+        verticalalignment = 'bottom'
+        y = y + .002
+    else:
+        verticalalignment = 'top'
+        y = y - .002
+    plt.text(x, y, name, size=10,
+             horizontalalignment=horizontalalignment,
+             verticalalignment=verticalalignment,
+             bbox=dict(facecolor='w',
+                       edgecolor=plt.cm.spectral(label / float(n_labels)),
+                       alpha=.6))
+
+plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
+         embedding[0].max() + .10 * embedding[0].ptp(),)
+plt.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
+         embedding[1].max() + .03 * embedding[1].ptp())
+
+plt.title('上证50成分股')
+plt.show()
---
+++ MathAn/PlotConsData.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams['font.sans-serif'] = ['KaiTi']
+
+
+ConsName = '中证水泥'
+df = pd.read_csv('f:/StocksData/Cons/' +ConsName + '.csv', index_col=0, dtype={'st_code':object})
+df['open_round'] = df.open_chg.round(0)
+df['day_round'] = df.day_chg.round(0)
+
+dd = df[['open_chg', 'day_chg', 'close']].groupby(['day_chg', 'open_chg']).count().reset_index()
+# dd = df[['open_round', 'day_round', 'close']].groupby(['day_round', 'open_round']).count().reset_index()
+
+# dd.to_csv('f:/group.csv')
+# data = dd.set_index('open_chg')
+
+pt = dd.pivot(index='open_chg', columns='day_chg', values='close')
+# pt = dd.pivot(index='open_round', columns='day_round', values='close')
+f, ax = plt.subplots(figsize = (16,9))
+
+sns.heatmap(pt, cmap = 'cool',linewidths=0.1, linecolor=(0.1, 0.2, 0.5, 0.05))  
+ax.set_title(ConsName)
+ax.set_xlabel('day_chg')
+ax.set_ylabel('open_chg')
+
+plt.show()
