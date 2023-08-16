27e65350162f654448a12bf33f876b575012676e
u588 <u588@sina.com>
2023-08-16 17:28:10 +0800

    o

--- MathAn/Stockplt.py
+++ MathAn/Stockplt.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
+import mplfinance as mpf
 
 from sqlalchemy import create_engine
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/tdxStocks')
 
-stockID = '600072'
+stockID = '601108'
 df = pd.read_sql(stockID, eng).tail(233).reset_index(drop=True).reset_index()
 
 fig, ax = plt.subplots()
