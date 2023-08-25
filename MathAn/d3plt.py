81e27954dc9e87565539c229d1ff6fe48874ba54
u588 <u588@sina.com>
2023-08-25 15:36:29 +0800

    o

---
+++ MathAn/d3plt.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn import preprocessing
+from sqlalchemy import create_engine
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/tdxStocks')
+
+df = pd.read_sql('600409', eng).tail(250).reset_index(drop=True).reset_index()
+
+
+# Load d3blocks
+from d3blocks import D3Blocks
+#
+# Initialize
+d3 = D3Blocks()
+#
+# Load example data
+# df = d3.import_example('cancer')
+#
+# Set size and tooltip
+
+# size = df['survival_months'].fillna(1).values / 20
+size =  ((preprocessing.minmax_scale(df.vol))*38).round(2)
+# tooltip = df['labx'].values + ' <br /> Survival: ' + df['survival_months'].astype(str).str[0:4].values
+tooltip = 'Date: '+df['datetime'].str[:10]+' <br /> Close: '+df['close'].astype(str).values + ' <br /> vol: ' + df['vol'].astype(str).values
+#
+# Scatter plot
+d3.scatter(df['index'].values,
+               df['close'].values,
+               size=size,
+               color=df['close'].astype(str).values,
+               stroke='#000000',
+               opacity=0.4,
+               tooltip=tooltip,
+            #    scale='true',
+               title='D3Plt',
+               figsize=[1200,600],
+               filepath='scatter_demo.html',
+               cmap='tab20c')
+
