e1a950b3d1be4187613b18282b8550b2fa1446a6
u588 <u588@sina.com>
2023-11-27 10:24:17 +0800

    o

--- MathAn/DataAn.py
+++ MathAn/DataAn.py
@@ -22,6 +22,7 @@ d5 = StocksList[4*n:]
 
 
 ss = pd.DataFrame(columns=['code'], dtype=object)
+sss = pd.DataFrame(columns=['code'], dtype=object)
 
 def getlist(lis):
     sl = pd.DataFrame(columns=['code'], dtype=object)
@@ -38,22 +39,61 @@ def getlist(lis):
             pass
     return sl
 
+def getlistS(lis):
+    sl = pd.DataFrame(columns=['code'], dtype=object)
+    for i, j in enumerate(lis):
+        try:
+            df = pd.read_sql(j, eng)
+            if (df.close.tail(20).tail(1).to_list()[0]-df.close.tail(20).head(1).to_list()[0]) > 0:
+                sl.loc[i,'code'] = j
+            else:
+                # print(j)
+                pass
+        except:
+            pass
+    return sl
+
 def Merg(res):
     global ss
     a = res.result()
     ss = pd.concat([ss, a])
     return ss
 
-data = [d1,d2,d3,d4,d5]
+def MergS(res):
+    global sss
+    a = res.result()
+    sss = pd.concat([sss, a])
+    return sss
+
+data1 = [d1,d2,d3,d4,d5]
 
 def MultiGet(workers, jobs):
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as pool:
         for task in jobs:
             pool.submit(getlist, task).add_done_callback(Merg)
 
+def MultiGetS(workers, jobs):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as pool:
+        for task in jobs:
+            pool.submit(getlistS, task).add_done_callback(MergS)
+
+
 if __name__ == '__main__':
 
-    MultiGet(5,data)
-    print(ss)
+    MultiGet(5,data1)
+    ss.reset_index(drop=True, inplace=True)
+    m = int(len(ss)/5)
+    dd1 = StocksList[:m]
+    dd2 = StocksList[m:2*m]
+    dd3 = StocksList[2*m:3*m]
+    dd4 = StocksList[3*m:4*m]
+    dd5 = StocksList[4*m:]
+    data2 = [dd1,dd2,dd3,dd4,dd5]
+
+    MultiGetS(5,data2)
+
+
+
+    print(len(sss)/len(ss))
 
     
