076c16c6641f49a70d79307e889f1d93b66f2661
u588 <u588@sina.com>
2023-12-06 17:11:39 +0800

    o

--- MathAn/timeAn/StGetPCBTab.py
+++ MathAn/timeAn/StGetPCBTab.py
@@ -4,12 +4,15 @@ from sqlalchemy import create_engine
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/tdxStocks')
 engAn = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/DataAn')
 
-df = pd.read_sql('000001', eng)
+code = '000001'
+rawD = pd.read_sql(code, eng)
+df = rawD[rawD.datetime >= '2000-01-01'].reset_index(drop=True)
 eng.dispose()
 
-p0 = [[3,8],[5,13],[13,34],[21,55]]
+p0 = [[5,13],[13,34]]
 
-def GetPCB(df,n,p):
+#第二个参数n涨幅周期，第三个参数p回测周期。
+def GetPCBraw(df,n,p):
     a = ((df.loc[n].close - df.loc[(n-p)].close)/df.loc[(n-p)].close)*100
     return a.round(2)
 
@@ -18,7 +21,7 @@ for n in df.index[::-1]:
     try:
         for p in p0:
             try:
-                df.loc[n, 'PCB'+str(p[0])] = GetPCB(df,n,p[0])
+                df.loc[n, 'PCB'+str(p[0])] = GetPCBraw(df,n,p[0])
                 df.loc[n,'PCB'+str(p[0])+'time'] = df.loc[(n-p[0])].datetime
                 df.loc[n,'PCB'+str(p[0])+'time'+str(p[1])] = df.loc[(n-(p[0]+p[1]))].datetime
             except:
@@ -26,11 +29,10 @@ for n in df.index[::-1]:
     except:
         pass
 
-pcb3 = df[['datetime', 'PCB3','PCB3time', 'PCB3time8']]
 pcb5 = df[['datetime', 'PCB5','PCB5time', 'PCB5time13']]
 pcb13 = df[['datetime', 'PCB13','PCB13time', 'PCB13time34']]
-pcb21 = df[['datetime', 'PCB21','PCB21time', 'PCB21time55']]
 
+#第二个参数m涨幅周期，第三个参数选择涨幅大于g的记录。
 def GetPCB(pcb,m,g):
     dd = pd.DataFrame()
     n = 0
@@ -51,9 +53,10 @@ def GetPCB(pcb,m,g):
 
 
 # df.to_sql('Index0', engAn, if_exists='replace')
-pcb = [[pcb3,3,4],[pcb5,5,6],[pcb13,13,10],[pcb21,21,15]]
+
+pcb = [[pcb5,5,8],[pcb13,13,16]]
 for n  in pcb:
     a = GetPCB(n[0],n[1],n[2]).reset_index(drop=True)
     a['Num'] = pd.to_datetime(a.datetime)-pd.to_datetime(a.shift(1).datetime)
-    a.to_excel('g:/1/StPCB'+str(n[1])+'.xlsx')
-# df.to_excel('g:/1/stock0.xlsx')
+    a.to_excel('g:/1/2/St'+code + 'PCB'+str(n[1])+'.xlsx')
+df.to_excel('g:/1/2/St'+code + '.xlsx')
