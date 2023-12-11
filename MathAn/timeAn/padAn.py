ce2f37b17063bfc8917b47836c66dd4dd7605ca6
u588 <u588@sina.com>
2023-12-11 18:19:33 +0800

    0

--- MathAn/timeAn/padAn.py
+++ MathAn/timeAn/padAn.py
@@ -4,9 +4,22 @@ a = pd.read_excel('g:/1/2/st000001.xlsx')
 b = pd.read_excel('g:/1/2/st000001pcb5.xlsx')
 c = pd.read_excel('g:/1/2/st000001pcb13.xlsx')
 
-a[a.datetime>=b.loc[1][2:5][1]][a.datetime<=b.loc[1][2:5][0]].reset_index().amount
 
-def CvCore(df,n,m):
-    a = df[df.columns[:n]][:m].mean().mean()*df[df.columns[:n]][:m].mean().std()
+def CvCore(df):
+    a = (df.mean()*df.std()).mean()
     return a
+
+#pcb5取分析时间
+i= 0
+while i <= len(b):
+    print(i)
+    try:
+        df = a[a.datetime>=b.loc[i][2:5][1]][a.datetime<=b.loc[i][2:5][0]].reset_index()[['open','close','high','low','mea']]
+        b.loc[i,'Cv'] = CvCore(df)
+        i = i + 1 
+    except:
+        i = i +1
+        pass
+
+
 
