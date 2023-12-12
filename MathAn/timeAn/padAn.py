0a1891cd36f16a5c137f25d8b268af96d746ca92
u588 <u588@sina.com>
2023-12-12 15:10:50 +0800

    k

--- MathAn/timeAn/padAn.py
+++ MathAn/timeAn/padAn.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+
 
 a = pd.read_excel('g:/1/2/st000001.xlsx')
 b = pd.read_excel('g:/1/2/st000001pcb5.xlsx')
@@ -14,7 +16,7 @@ i= 0
 while i <= len(b):
     print(i)
     try:
-        df = a[a.datetime>=b.loc[i][2:5][1]][a.datetime<=b.loc[i][2:5][0]].reset_index()[['open','close','high','low','mea']]
+        df = a[a.datetime>=b.loc[i][3:5][1]][a.datetime<=b.loc[i][3:5][0]].reset_index()[['open','close','high','low','mea']]
         b.loc[i,'Cv'] = CvCore(df)
         i = i + 1 
     except:
