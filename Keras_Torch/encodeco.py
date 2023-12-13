a857a0ea93fa76b9d1817e4538129717d2cdb0df
u588 <u588@sina.com>
2023-12-13 16:50:40 +0800

    k

--- Keras_Torch/encodeco.py
+++ Keras_Torch/encodeco.py
@@ -7,14 +7,17 @@ os.environ["KERAS_BACKEND"] = "torch"
 import torch
 import keras
 from keras import layers
+from keras import models
+from keras.models import load_model
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as pltk
 
 a = pd.read_excel('g:/1/2/st000001.xlsx')
 b = pd.read_excel('g:/1/2/st000001pcb5.xlsx')
 c = pd.read_excel('g:/1/2/st000001pcb13.xlsx')
-
 
 i = 0
 qq = pd.DataFrame((a[a.datetime>=b.loc[i][3:5][1]][a.datetime<=b.loc[i][3:5][0]].reset_index()[['open','close','high','low','mea']]).stack().values).T
@@ -29,28 +32,22 @@ nor = Normalizer(norm='l2')
 x_train = nor.fit_transform(qq[1:65])
 x_test = nor.fit_transform(qq[65:])
 
-encoding_dim = 10  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-# This is our input image
+encoding_dim = 30 
 input_img = keras.Input(shape=(70,))
-# "encoded" is the encoded representation of the input
 encoded = layers.Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
 decoded = layers.Dense(70, activation='sigmoid')(encoded)
-# This model maps an input to its reconstruction
 autoencoder = keras.Model(input_img, decoded)
-# This model maps an input to its encoded representation
+
 encoder = keras.Model(input_img, encoded)
-# This is our encoded (32-dimensional) input
+
 encoded_input = keras.Input(shape=(encoding_dim,))
-# Retrieve the last layer of the autoencoder model
 decoder_layer = autoencoder.layers[-1]
-# Create the decoder model
 decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
-
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=100,
                 batch_size=20,
                 shuffle=True,
                 validation_data=(x_test, x_test))
@@ -60,17 +57,17 @@ autoencoder.fit(x_train, x_train,
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
-import matplotlib.pyplot as plt
+
 n = 20  # How many digits we will display
-plt.figure(figsize=(20, 4))
+plt.figure(figsize=(12, 4))
 for i in range(n):
-    # Display original
+    # Top Display original
     ax = plt.subplot(2, n, i + 1)
     plt.imshow(x_test[i].reshape(14, 5))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
-    # Display reconstruction
+    # Bottom Display reconstruction
     ax = plt.subplot(2, n, i + 1 + n)
     plt.imshow(decoded_imgs[i].reshape(14, 5))
     plt.gray()
@@ -79,7 +76,7 @@ for i in range(n):
 
 plt.show()
 
-
+# ============= ==================
 
 from keras import regularizers
 
@@ -105,146 +102,16 @@ autoencoder = keras.Model(input_img, decoded)
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=100,
                 batch_size=20,
                 shuffle=True,
                 validation_data=(x_test, x_test))
 
-import keras
-from keras import layers
+# ================  ===============================
 
-input_img = keras.Input(shape=(28, 28, 1))
-
-x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-x = layers.MaxPooling2D((2, 2), padding='same')(x)
-x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = layers.MaxPooling2D((2, 2), padding='same')(x)
-x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
-
-# at this point the representation is (4, 4, 8) i.e. 128-dimensional
-
-x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
-x = layers.UpSampling2D((2, 2))(x)
-x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = layers.UpSampling2D((2, 2))(x)
-x = layers.Conv2D(16, (3, 3), activation='relu')(x)
-x = layers.UpSampling2D((2, 2))(x)
-decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-
-autoencoder = keras.Model(input_img, decoded)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-
-from keras.datasets import mnist
-import numpy as np
-
-(x_train, _), (x_test, _) = mnist.load_data()
-
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
-
-from keras.callbacks import TensorBoard
-
-autoencoder.fit(x_train, x_train,
-                epochs=50,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='g:/tmp')])
-
-decoded_imgs = autoencoder.predict(x_test)
-
-n = 10
-plt.figure(figsize=(20, 4))
-for i in range(1, n + 1):
-    # Display original
-    ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    # Display reconstruction
-    ax = plt.subplot(2, n, i + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-plt.show()
-
-encoder = keras.Model(input_img, encoded)
-encoded_imgs = encoder.predict(x_test)
-
-n = 10
-plt.figure(figsize=(20, 8))
-for i in range(1, n + 1):
-    ax = plt.subplot(1, n, i)
-    plt.imshow(encoded_imgs[i].reshape((4, 4 * 8)).T)
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-plt.show()
-
-from keras.datasets import mnist
-import numpy as np
-
-(x_train, _), (x_test, _) = mnist.load_data()
-
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
-
-noise_factor = 0.5
-x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
-x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_test_noisy = np.clip(x_test_noisy, 0., 1.)
-
-n = 10
-plt.figure(figsize=(20, 2))
-for i in range(1, n + 1):
-    ax = plt.subplot(1, n, i)
-    plt.imshow(x_test_noisy[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
-input_img = keras.Input(shape=(28, 28, 1))
-
-x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-x = layers.MaxPooling2D((2, 2), padding='same')(x)
-x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
-
-# At this point the representation is (7, 7, 32)
-
-x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
-x = layers.UpSampling2D((2, 2))(x)
-x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = layers.UpSampling2D((2, 2))(x)
-decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-
-autoencoder = keras.Model(input_img, decoded)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-autoencoder.fit(x_train_noisy, x_train,
-                epochs=100,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test_noisy, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=0, write_graph=False)])
-
-
-timesteps = ...  # Length of your sequences
-input_dim = ... 
-latent_dim = ...
+timesteps = 13  
+input_dim = 70 
+latent_dim = 5
 
 inputs = keras.Input(shape=(timesteps, input_dim))
 encoded = layers.LSTM(latent_dim)(inputs)
@@ -256,7 +123,7 @@ sequence_autoencoder = keras.Model(inputs, decoded)
 encoder = keras.Model(inputs, encoded)
 
 
-original_dim = 28 * 28
+original_dim = 70
 intermediate_dim = 64
 latent_dim = 2
 
