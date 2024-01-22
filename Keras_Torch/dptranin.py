eb027243256a788dc182bb5796dd2d23905cf7e0
u588 <u588@sina.com>
2024-01-22 13:53:14 +0800

    v

---
+++ Keras_Torch/dptranin.py
@@ -0,0 +1,154 @@
+# https://keras.io/guides/distributed_training_with_torch/
+
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+
+import torch
+import numpy as np
+import keras
+
+
+def get_model():
+    # Make a simple convnet with batch normalization and dropout.
+    inputs = keras.Input(shape=(28, 28, 1))
+    x = keras.layers.Rescaling(1.0 / 255.0)(inputs)
+    x = keras.layers.Conv2D(filters=12, kernel_size=3, padding="same", use_bias=False)(x)
+    x = keras.layers.BatchNormalization(scale=False, center=True)(x)
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.Conv2D(
+        filters=24,
+        kernel_size=6,
+        use_bias=False,
+        strides=2,
+    )(x)
+    x = keras.layers.BatchNormalization(scale=False, center=True)(x)
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.Conv2D(
+        filters=32,
+        kernel_size=6,
+        padding="same",
+        strides=2,
+        name="large_k",
+    )(x)
+    x = keras.layers.BatchNormalization(scale=False, center=True)(x)
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dense(256, activation="relu")(x)
+    x = keras.layers.Dropout(0.5)(x)
+    outputs = keras.layers.Dense(10)(x)
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+def get_dataset():
+    # Load the data and split it between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32")
+    x_test = x_test.astype("float32")
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    # Create a TensorDataset
+    dataset = torch.utils.data.TensorDataset(
+        torch.from_numpy(x_train), torch.from_numpy(y_train)
+    )
+    return dataset
+
+
+def train_model(model, dataloader, num_epochs, optimizer, loss_fn):
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        running_loss_count = 0
+        for batch_idx, (inputs, targets) in enumerate(dataloader):
+            inputs = inputs.cuda(non_blocking=True)
+            targets = targets.cuda(non_blocking=True)
+            # Forward pass
+            outputs = model(inputs)
+            loss = loss_fn(outputs, targets)
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            running_loss_count += 1
+        # Print loss statistics
+        print(
+            f"Epoch {epoch + 1}/{num_epochs}, "
+            f"Loss: {running_loss / running_loss_count}"
+        )
+
+
+
+num_gpu = torch.cuda.device_count()
+num_epochs = 2
+batch_size = 64
+print(f"Running on {num_gpu} GPUs")
+
+
+def setup_device(current_gpu_index, num_gpus):
+    # Device setup
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "56492"
+    device = torch.device("cuda:{}".format(current_gpu_index))
+    torch.distributed.init_process_group(
+        backend="nccl",
+        init_method="env://",
+        world_size=num_gpus,
+        rank=current_gpu_index,
+    )
+    torch.cuda.set_device(device)
+
+
+def cleanup():
+    torch.distributed.destroy_process_group()
+
+
+def prepare_dataloader(dataset, current_gpu_index, num_gpus, batch_size):
+    sampler = torch.utils.data.distributed.DistributedSampler(
+        dataset,
+        num_replicas=num_gpus,
+        rank=current_gpu_index,
+        shuffle=False,
+    )
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+    return dataloader
+
+
+def per_device_launch_fn(current_gpu_index, num_gpu):
+    # Setup the process groups
+    setup_device(current_gpu_index, num_gpu)
+    dataset = get_dataset()
+    model = get_model()
+    # prepare the dataloader
+    dataloader = prepare_dataloader(dataset, current_gpu_index, num_gpu, batch_size)
+    # Instantiate the torch optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    # Instantiate the torch loss function
+    loss_fn = torch.nn.CrossEntropyLoss()
+    # Put model on device
+    model = model.to(current_gpu_index)
+    ddp_model = torch.nn.parallel.DistributedDataParallel(
+        model, device_ids=[current_gpu_index], output_device=current_gpu_index
+    )
+    train_model(ddp_model, dataloader, num_epochs, optimizer, loss_fn)
+    cleanup()
+
+
+if __name__ == "__main__":
+    # We use the "fork" method rather than "spawn" to support notebooks
+    torch.multiprocessing.start_processes(
+        per_device_launch_fn,
+        args=(num_gpu,),
+        nprocs=num_gpu,
+        join=True,
+        start_method="fork",
+    )
+
--- Keras_Torch/test.py
+++ Keras_Torch/test.py
@@ -3,6 +3,7 @@ import os
 
 # This guide can only be run with the torch backend.
 os.environ["KERAS_BACKEND"] = "torch"
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
 import torch
 import keras
@@ -14,33 +15,26 @@ class CustomModel(keras.Model):
         # Unpack the data. Its structure depends on your model and
         # on what you pass to `fit()`.
         x, y = data
-
         # Call torch.nn.Module.zero_grad() to clear the leftover gradients
         # for the weights from the previous train step.
         self.zero_grad()
-
         # Compute loss
         y_pred = self(x, training=True)  # Forward pass
         loss = self.compute_loss(y=y, y_pred=y_pred)
-
         # Call torch.Tensor.backward() on the loss to compute gradients
         # for the weights.
         loss.backward()
-
         trainable_weights = [v for v in self.trainable_weights]
         gradients = [v.value.grad for v in trainable_weights]
-
         # Update weights
         with torch.no_grad():
             self.optimizer.apply(gradients, trainable_weights)
-
         # Update metrics (includes the metric that tracks the loss)
         for metric in self.metrics:
             if metric.name == "loss":
                 metric.update_state(loss)
             else:
                 metric.update_state(y, y_pred)
-
         # Return a dict mapping metric names to current value
         # Note that it will include the loss (tracked in self.metrics).
         return {m.name: m.result() for m in self.metrics}
