43e8ece167546c798f46d4f46b383d2894a131ab
u588 <u588@sina.com>
2024-01-25 22:58:30 +0800

    v

--- Dask/ininode.py
+++ Dask/ininode.py
@@ -3,7 +3,8 @@ from dask_cuda import LocalCUDACluster
 cluster = LocalCluster(name='dask_6',n_workers=0,threads_per_worker=1,ip='10.3.69.7',scheduler_port='8786',
                        dashboard_address='10.3.69.7:8787',worker_dashboard_address='10.3.69.7',memory_limit='8GiB',
                        )
-cluster = LocalCUDACluster(name='dask_6',n_workers=2,threads_per_worker=1,ip='10.3.69.7',scheduler_port='8786',
-                       dashboard_address='10.3.69.7:8787',worker_dashboard_address='10.3.69.7',memory_limit='8GiB',
+cluster = LocalCUDACluster(name='dask_6',CUDA_VISIBLE_DEVICES='0,1',n_workers=2,threads_per_worker=1,ip='10.3.69.7',scheduler_port='8786',
+                       dashboard_address='10.3.69.7:8787',worker_dashboard_address='10.3.69.7',memory_limit='15GB',device_memory_limit=0.9,
+                       protocol='tcp',
                        )
 
