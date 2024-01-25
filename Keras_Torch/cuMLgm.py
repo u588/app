959177c70e028b05b23c0b3b5d84cab584fc4d60
u588 <u588@sina.com>
2024-01-25 21:28:42 +0800

    v

--- Keras_Torch/cuMLgm.py
+++ Keras_Torch/cuMLgm.py
@@ -7,7 +7,7 @@ import cudf
 
 engAn = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/DataAn')
 
-qq = pd.read_sql('qq3001',engAn.connect())
+qq = pd.read_sql('qq3002',engAn.connect())
 
 X = cudf.DataFrame(qq.fillna(1))
 model = DBSCAN(eps=0.48,min_samples=5)
