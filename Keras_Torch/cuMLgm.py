66ee7470f767045c5b1d53e5527185c832c44678
u588 <u588@sina.com>
2024-01-27 21:45:59 +0800

    v

--- Keras_Torch/cuMLgm.py
+++ Keras_Torch/cuMLgm.py
@@ -1,8 +1,14 @@
+# qq.astype('float32') 数据转 单精度
+# qq = qq.iloc[:,:36] 去前几列
+
+
+
 import pandas as pd
 from sqlalchemy import create_engine
-from cuml import DBSCAN
+# from cuml import DBSCAN
 from cuml.cluster import DBSCAN
-
+from cuml.common.device_selection import using_device_type, set_global_device_type
+set_global_device_type("CPU")
 import cudf
 
 engAn = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/DataAn')
@@ -10,6 +16,7 @@ engAn = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/DataAn'
 qq = pd.read_sql('qq3001',engAn.connect())
 
 X = cudf.DataFrame(qq.fillna(1))
+X = qq.fillna(1).values
 model = DBSCAN(eps=0.27,min_samples=5)
 
 yy = model.fit_predict(X)
