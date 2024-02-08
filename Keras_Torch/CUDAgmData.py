24cd9f1bbf311b1c8d443114bbf85af97f72e2cd
u588 <u588@sina.com>
2024-02-08 21:29:59 +0800

    v

--- Keras_Torch/CUDAgmData.py
+++ Keras_Torch/CUDAgmData.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import datetime
-import time
 from sqlalchemy import create_engine
 from cuml.dask.cluster import DBSCAN
 from dask.distributed import Client
 
 
 
-# li =[[300,1],[300,2],[500,1],[500,2],[1000,1],[1000,2],[2000,1],[2000,2]]
-li =[[1000,1],[1000,2],[2000,1],[2000,2]]
+li =[[300,1],[300,2],[500,1],[500,2],[1000,1],[1000,2],[2000,1],[2000,2]]
+# li =[[1000,1],[1000,2],[2000,1],[2000,2]]
 # li =[[500,2]]
 engAn = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/DataAn')
 for gm in li:
@@ -30,6 +29,9 @@ for gm in li:
         yy = model.fit_predict(X)
         t1 = datetime.datetime.now()
 
+        tt = int((t1-t0).total_seconds())
+        print('fit time: '+ str(tt))
+
         b['cluster'] = pd.DataFrame(yy)
         print(('b'+str(gm[0]) + str(gm[1])+'e'+str(esp)+'s8'))
 
@@ -37,13 +39,12 @@ for gm in li:
         xxg = xx.groupby('cluster')
 
         cl = xxg.PCB5.describe().sort_values(['25%','mean'],ascending=False).round(2).reset_index()
-        ccl = cl[cl['count']>5][cl['25%']>6].reset_index(drop=True)
+        ccl = cl[(cl['count']>5) & (cl['25%']>6)].reset_index(drop=True)
         n = ccl.shape[0]
         print(n)
         print(ccl)
-        print("\n")
         print("==============================================================")    
-
+        print("\n")
         if n > 40:
             esp = round(esp-0.1 , 3)
         elif n > 20:
@@ -57,11 +58,9 @@ for gm in li:
         else:
             pass
 
-        tt = int((t1-t0).total_seconds())
-        print(tt)
 
     client.restart_workers(['GTX','P4-0','P4-1'])
-    time.sleep(5)
+
 client.close()
 engAn.dispose()
 print('client closed !')
