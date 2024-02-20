3dd3a821e1e6059333a8508e93ba465e94a30d2e
u588 <u588@sina.com>
2024-02-20 10:22:49 +0800

    v

---
+++ TDXapp/MtdxGetFS.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from pytdx.hq import TdxHq_API
+from pytdx.crawler.base_crawler import demo_reporthook
+from pytdx.crawler.history_financial_crawler import HistoryFinancialCrawler
+
+
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxFS')
+api = TdxHq_API()
+api.connect('119.147.212.81', 7709)
+
+
+'''
+检查系统更新
+
+from pytdx.crawler.history_financial_crawler import HistoryFinancialListCrawler
+crawler = HistoryFinancialListCrawler()
+list_data = crawler.fetch_and_parse()
+print(pd.DataFrame(data=list_data))
+
+'''
+
+ls = ['gpcw20230930.zip']
+datacrawler = HistoryFinancialCrawler()
+pd.set_option('display.max_columns', None)
+
+for i in ls:
+    result = datacrawler.fetch_and_parse(reporthook=demo_reporthook, filename=i, path_to_download="/tmp/tmpfile.zip")
+    dd = datacrawler.to_df(data=result)
+    dd['report_date']= dd['report_date'].astype(object)
+    upday = dd['report_date'][0]
+    dd = dd.round(2)
+    for j,l in enumerate(dd.index.values.tolist()):
+        try:
+            day = pd.read_sql(l, eng)['report_date'].tail(1).tolist()[0]
+            if upday > day:
+                pd.DataFrame(dd.iloc[j]).T.reset_index(drop=True).set_index('report_date').to_sql(l, eng, if_exists='append')
+                
+            else:
+                print(l+'not Updated !')
+                pass
+        except:
+            print(l+" =====  Excepts !!")
+            pass
+
+eng.dispose()
+
