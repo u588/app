b08671063abec3b1ecbbedde197c13883c39460e
u588 <u588@sina.com>
2024-05-27 12:05:31 +0800

    v

--- www/apps/qInfo.py
+++ www/apps/qInfo.py
@@ -1,40 +1,26 @@
 import streamlit as st
 from streamlit_echarts import st_pyecharts
-from chart import Kpro,getCsIndex,csIndexChart,getCsStock,d3plt,detailChart
+from chart import Kpro,indexChart,d3plt,detailChart
 
 def app():
     with st.form('form1'):
         with st.sidebar:
-            cycCode = st.selectbox(
-                '分析周期选项',
-                ('3D','5D','21D','55D')
-            )
-            indexCode = getCsIndex.csIndexData(cycCode)
-            indexCodeSel = st.selectbox(
-                '指数选项',
-                (indexCode['IndexName'])
-            )
+            indexCode = st.text_input(label='指数查询',value='')
             submitted = st.form_submit_button('确认')
     if submitted:
-        st_pyecharts(csIndexChart.Kchart(indexCodeSel),height='600px')
+        st_pyecharts(indexChart.Kchart(indexCode),height='600px')
     
     with st.form('form2'):
         with st.sidebar:
-            stockCode = getCsStock.getStock(indexCodeSel,cycCode)
-            stockCodeSel = st.selectbox(
-                '股票选项',
-                (stockCode['code'])
-        )
+            stockCode = st.text_input(label='股票查询', value='')
             submitted1 = st.form_submit_button('确认')
     if submitted1:
         tab1,tab2 = st.tabs(['Kpro','D3plt'])
         with tab1:
-            # st.header('Kpro')         
-            st_pyecharts(Kpro.Kchart(stockCodeSel),height='750px')
+            st_pyecharts(Kpro.Kchart(stockCode),height='750px')
             st.header('财务分析')
-            st_pyecharts(detailChart.line(stockCodeSel))
+            st_pyecharts(detailChart.line(stockCode))
         with tab2:
-            # st.header('D3plt')
-            st.bokeh_chart(d3plt.d3(stockCodeSel),use_container_width=True)
+            st.bokeh_chart(d3plt.d3(stockCode),use_container_width=True)
 
             
