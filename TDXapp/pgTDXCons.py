22ef22f809fdcf82f2082db3a443d6d0d069fa7e
u588 <u588@sina.com>
2024-06-21 09:54:47 +0800

    v

--- TDXapp/pgTDXCons.py
+++ TDXapp/pgTDXCons.py
@@ -9,8 +9,11 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/tdxIndex')
 
 
-# sql = 'DROP TABLE IF EXISTS "csIndexCons";'
-eng.execute('DROP TABLE IF EXISTS "csIndexCons";')
+#from < v2.0
+# eng.execute('DROP TABLE IF EXISTS "csIndexCons";')
+
+# from v2.0
+eng.connect().execute('DROP TABLE IF EXISTS "csIndexCons";')
 time.sleep(5)
 
 def getData(codeID):
