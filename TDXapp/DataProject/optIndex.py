33d14d716fc2cfe4e2611259813de10d66b035ea
u588 <u588@sina.com>
2024-08-07 11:33:20 +0800

    v

--- TDXapp/DataProject/optIndex.py
+++ TDXapp/DataProject/optIndex.py
@@ -6,10 +6,10 @@ eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56:5432/tdxIndex'
 tdxIndex = pd.read_sql('tdxIndexs', eng)
 empIndex = pd.read_sql('EmpIndex', eng)
 
-df = tdxIndex.append(empIndex).drop_duplicates(subset=['IndexCode'], keep=False).drop('index', axis=1)
+df = pd.concat([tdxIndex,empIndex]).drop_duplicates(subset=['IndexCode'], keep=False).drop('index', axis=1)
 
 
-# df.set_index('IndexCode').to_sql('tdxIndexs',eng, if_exists = 'replace')
+df.set_index('IndexCode').to_sql('optIndexs',eng, if_exists = 'replace')
 
 df.set_index('IndexCode').to_excel('G:/Gitee/App/TDXapp/tdxAppData/optIndexs.xlsx')
 print('Saved ! ')
