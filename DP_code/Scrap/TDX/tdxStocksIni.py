d430b071c8b6dc493ca3482b872f24325af6c6e1
u588 <u588@sina.com>
2024-08-13 08:50:09 +0800

    v

--- DP_code/Scrap/TDX/tdxStocksIni.py
+++ DP_code/Scrap/TDX/tdxStocksIni.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 api = TdxHq_API()
 api = TdxHq_API(heartbeat=True)
 
-eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.55:5432/tdxStocks')
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.145.254.56/tdxStocks')
 StockLists =  pd.read_sql('StocksList', eng).code.tolist()
 
 with api.connect('119.147.212.81', 7709):
@@ -17,13 +17,15 @@ with api.connect('119.147.212.81', 7709):
             nn = 1
         else:
             nn = 0    
+        try:
+            start = 5500
+            while start >= 0:
+                df = api.to_df(api.get_security_bars(9, nn, StockCode, start, 500))
+                start = start - 500
+                StockData = pd.concat([StockData,df])
 
-        start = 5500
-        while start >= 0:
-            df = api.to_df(api.get_security_bars(9, nn, StockCode, start, 500))
-            start = start - 500
-            StockData = pd.concat([StockData,df])
-
-        StockData.set_index('datetime', inplace=True)
-        StockData.to_sql(StockCode, eng, if_exists='replace')
+            StockData.set_index('datetime', inplace=True)
+            StockData.to_sql(StockCode, eng, if_exists='replace')
+        except:
+            pass
         print(StockCode,'saved to sql !')
