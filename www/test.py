807ef7c391d3da7462d1b88c1e01dc3c12d1ea0b
u588 <u588@sina.com>
2024-08-19 17:10:06 +0800

    v

--- www/test.py
+++ www/test.py
@@ -1,23 +1,23 @@
-from pygwalker.api.streamlit import init_streamlit_comm, StreamlitRenderer
 import pandas as pd
 from sqlalchemy import create_engine
 import streamlit as st
-eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxStocks')
-# Initialize pygwalker communication
-init_streamlit_comm()
+
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxFS')
+
+import plotly.graph_objects as go
+
+fig =go.Figure(go.Sunburst(
+    labels=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+    parents=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+    values=[10, 14, 12, 10, 2, 6, 6, 4, 4],
+))
+# Update layout for tight margin
+# See https://plotly.com/python/creating-and-updating-figures/
+fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
 
 
-# You should cache your pygwalker renderer, if you don't want your memory to explode
-@st.cache_resource
-def get_pyg_renderer() -> "StreamlitRenderer":
-    df = pd.read_sql('000001', eng).tail(20)
-    # When you need to publish your application, you need set `debug=False`,prevent other users to write your config file.
-    return StreamlitRenderer(df, spec="./billion_config.json", debug=True)
-
-renderer = get_pyg_renderer()
-
-# Display explore ui, Developers can use this to prepare the charts you need to display.
-renderer.explorer()
-
-# Display pure chart, index is the order of charts in explore mode, starting from 0.
-# renderer.chart(0)
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    st.plotly_chart(fig, theme="streamlit")
+with tab2:
+    st.plotly_chart(fig, theme=None)
