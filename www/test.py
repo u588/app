c579f0991bede2d5d441eaa10b0239c76bdb759a
u588 <u588@sina.com>
2024-08-20 17:07:23 +0800

    v

--- www/test.py
+++ www/test.py
@@ -4,15 +4,17 @@ import streamlit as st
 
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxFS')
 
-FSCode = pd.read_sql('tdxFSCode',eng)
+FSCode = pd.read_sql('FSCode',eng)
 stockCode = '600409'
+day = 20240331
+
 finRAW = pd.read_sql(stockCode, eng)
 finRAW['Index']=finRAW['report_date']
 trsfin = finRAW.set_index('Index').T
 trsfin = trsfin.reset_index().rename(columns={'index':'Code'})
 
 sfin = pd.merge(FSCode,trsfin,on='Code',how='inner')
-day = 20240331
+
 ll = ['Index','L1Code','L1Name','L2Code','L2Name','L3Code','L3Name','Code','cnName',day]
 fin = sfin[ll]
 items = fin.cnName.to_list()
@@ -30,7 +32,7 @@ import plotly.express as px
 fig = px.sunburst(zcfz, path=['L2Name','L3Name','cnName'], values=day)
 
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+tab1, tab2 = st.tabs([stockCode+" : Streamlit theme (default)", stockCode+" : Plotly native theme"])
 with tab1:
     st.plotly_chart(fig, theme="streamlit")
 with tab2:
