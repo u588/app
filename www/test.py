73349ac1622a25a65c189c8dfe2c6509ee5a6c62
u588 <u588@sina.com>
2024-08-21 11:22:24 +0800

    v

--- www/test.py
+++ www/test.py
@@ -5,32 +5,40 @@ import streamlit as st
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxFS')
 
 FSCode = pd.read_sql('FSCode',eng)
-stockCode = '002202'
+stockCode = '000017'
 day = 20240331
 
 finRAW = pd.read_sql(stockCode, eng)
-finRAW['Index']=finRAW['report_date']
+finRAW = pd.concat([finRAW,finRAW['report_date'].rename('Index')],axis=1)
 trsfin = finRAW.set_index('Index').T
 trsfin = trsfin.reset_index().rename(columns={'index':'Code'})
 
 sfin = pd.merge(FSCode,trsfin,on='Code',how='inner')
 
-ll = ['Index','L1Code','L1Name','L2Code','L2Name','L3Code','L3Name','Code','cnName',day]
-fin = sfin[ll]
+ll = ['Index','L1Code','L1Name','L2Code','L2Name','L3Code','L3Name','Code','cnName']
+fin = pd.concat([sfin[ll],sfin[day].rename('vol')],axis=1)
 items = fin.cnName.to_list()
 sumite = [item for item in items if any(substr in item for substr in "万")]
 
 for ite in sumite:
-    fin.loc[fin.cnName==ite,day] = fin[fin.cnName==ite][day]*10000
-
+    fin.loc[fin.cnName==ite,"vol"] = fin[fin.cnName==ite]["vol"]*10000
 zcfz = fin.query('L1Code=="ZCFZ" and L3Code!="EMP" and L3Code!="HJ"')
 
-zcfz = zcfz[~(zcfz[day]==0)]
+zcfz = zcfz[~(zcfz["vol"]==0)].reset_index(drop=True)
+# zcfz.loc[:,"vol"]=abs(zcfz["vol"])
+for i,ite in enumerate(zcfz.vol.to_list()):
+    if ite < 0:
+        zcfz.loc[i,'cnName'] ='负：'+ zcfz.loc[i,'cnName']
+        zcfz.loc[i,'vol'] = abs(ite) 
+    else:
+        pass
+
+
 
 import plotly.express as px
 
-fig = px.sunburst(zcfz, path=['L2Name','L3Name','cnName'], values=day)
-
+fig = px.sunburst(zcfz, path=['L2Name','L3Name','cnName'], values="vol")
+fig.update_layout(title=stockCode)
 
 tab1, tab2 = st.tabs([stockCode+' : '+str(day)+" : Streamlit theme (default)", stockCode+" : Plotly native theme"])
 with tab1:
