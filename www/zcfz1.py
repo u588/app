3716c553559286bd9e70a9d125151c81ed24d3c3
u588 <u588@sina.com>
2024-09-10 16:01:19 +0800

    v

--- www/zcfz1.py
+++ www/zcfz1.py
@@ -33,39 +33,21 @@ for i,ite in enumerate(zcfz.vol.to_list()):
     else:
         pass
 
-# zzcfz = fin.query('L1Code=="ZCFZ" and (L3Code=="HJ" or L3Code=="SSGD")')
-# zzcfz = zzcfz[~(zzcfz["vol"]==0)].reset_index(drop=True)
-
-# for i,ite in enumerate(zzcfz.vol.to_list()):
-#     if ite < 0:
-#         zzcfz.loc[i,'cnName'] ='负：'+ zzcfz.loc[i,'cnName']
-#         zzcfz.loc[i,'vol'] = abs(ite) 
-#     else:
-#         pass
-
-
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-# colors = plotly.colors.qualitative.Light24
-# colors = plotly.colors.sequential.Plotly3
+
 
 df = zcfz
 
 levels = ['cnName', 'L3Name', 'L2Name'] # levels used for the hierarchical chart
-# color_columns = ['vol']
+
 value_column = 'vol'
 
 def build_hierarchical_dataframe(df, levels, value_column):
-    """
-    Build a hierarchy of levels for Sunburst or Treemap charts.
-
-    Levels are given starting from the bottom to the top of the hierarchy,
-    ie the last level corresponds to the root.
-    """
     df_list = []
     for i, level in enumerate(levels):
-        # df_tree = pd.DataFrame(columns=['id', 'parent', 'value', 'color'])
+
         df_tree = pd.DataFrame(columns=['id', 'parent', 'value'])
         dfg = df.groupby(levels[i:]).sum()
         dfg = dfg.reset_index()
@@ -75,21 +57,17 @@ def build_hierarchical_dataframe(df, levels, value_column):
         else:
             df_tree['parent'] = 'total'
         df_tree['value'] = dfg[value_column]
-        # df_tree['color'] = dfg[color_columns[0]] / dfg[color_columns[1]]
+
         df_list.append(df_tree)
-    # total = pd.Series(dict(id='total', parent='',
-    #                           value=df[value_column].sum(),
-    #                           color=df[color_columns[0]].sum() / df[color_columns[1]].sum()), name=0)
-    # df_list.append(total)
+
     df_all_trees = pd.concat(df_list, ignore_index=True)
     return df_all_trees
 
 
 df_all_trees = build_hierarchical_dataframe(df, levels, value_column)
-# df_all_trees = build_hierarchical_dataframe(df, levels, value_column, color_columns)
-# average_score = df['sales'].sum() / df['calls'].sum()
 
-fig = make_subplots(1, 2, specs=[[{"type": "domain"}, {"type": "domain"}]],)
+
+fig = make_subplots(1, 2, specs=[[{"type": "domain"}, {"type": "domain"}]])
 
 fig.add_trace(go.Sunburst(
     labels=df_all_trees['id'],
@@ -98,13 +76,6 @@ fig.add_trace(go.Sunburst(
     # branchvalues='total',
     branchvalues='remainder',
     textinfo='label +percent root',
-    # marker_colors=colors,
-    # marker=dict(
-    #     colors=df_all_trees['color'],
-    #     colorscale='RdBu',
-    #     cmid=average_score),
-    # hovertemplate='<b>%{label} </b> <br> Sales: %{value}<br> Success rate: %{color:.2f}',
-
     ), 1, 2)
 
 fig.add_trace(go.Sunburst(
@@ -115,15 +86,8 @@ fig.add_trace(go.Sunburst(
     # branchvalues='remainder',
     branchvalues='total',
     name='2',
-    
-    # marker_colors=colors,
-    # marker=dict(
-    #     colors=df_all_trees['color'],
-    #     colorscale='RdBu',
-    #     cmid=average_score),
-    # hovertemplate='<b>%{label} </b> <br> Sales: %{value}<br> Success rate: %{color:.2f}',
     maxdepth=3
     ), 1, 1)
 traces=fig.data
-fig.update_layout(margin=dict(t=0, b=0, r=0, l=0))
+fig.update_layout(margin=dict(t=50, b=0, r=0, l=0),title_text=stockCode,title_x=0.5,title_font_size=25)
 st.plotly_chart(fig)
