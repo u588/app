458d2b0e9ab76c982db6a91535fd55789472d21f
u588 <u588@sina.com>
2024-10-01 21:58:22 +0800

    v

---
+++ TDXapp/tdxGetFcast.py
@@ -0,0 +1,56 @@
+from mootdx.quotes import Quotes
+import pandas as pd
+import re
+from sqlalchemy import create_engine
+
+eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/StockBas')
+engs = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxStocks')
+
+
+
+def getTop(StockCode, StockName):
+    qf10='价值分析'
+    client = Quotes.factory(market='std')
+    txtRaw = client.F10(StockCode, qf10)[116:]
+
+    txt = txtRaw.replace('│',' ')                
+    txt = re.sub('([\u2500-\u25f7])','',txt)
+    
+    ftxt = txt[txt.find('【2.盈利预测统计】'):]
+    etxt = ftxt[:ftxt.find('【3.盈利预测明细】')]
+    dd = etxt.splitlines(keepends=False)
+
+    Data = pd.DataFrame(columns=())
+    i = 0
+    while i < len(dd):
+        lis = dd[i].split()
+        if len(lis)!=7:
+            i = i+1
+            # pass
+        else:
+            df = pd.DataFrame(lis).T
+            # df.columns=["股票名称", "一周涨跌幅%","一月涨跌幅%","三月涨跌幅%","半年涨跌幅%","一年涨跌幅%"]
+            Data = pd.concat([Data, df],axis=0)
+            i=i+1
+    Data.reset_index(drop=True,inplace=True)
+    Data = Data.replace('---',pd.NA)
+
+    Data.columns=[Data.loc[0]]
+    Data['StockCode'] = StockCode
+    Data['StockName'] = StockName
+
+    Data.tail(-1).set_index('StockCode').to_sql('Fcast', eng, if_exists='append')
+
+
+StockList = pd.read_sql('StocksList', engs)[['code','name']]
+n = 0
+while n < len(StockList):
+    try:
+        getTop(StockList.iloc[n,0], StockList.iloc[n,1])
+        print(StockList.iloc[n,0]+ 'OK !')
+        
+    except:
+        print(StockList.iloc[n,0] + 'Failure ! ')
+       
+        pass
+    n = n + 1
