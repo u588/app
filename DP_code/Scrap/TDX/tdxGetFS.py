147046aaa902a5461cd1975bc2a88b08c4a31976
u588 <u588@sina.com>
2025-05-07 14:25:56 +0800

    v

--- DP_code/Scrap/TDX/tdxGetFS.py
+++ DP_code/Scrap/TDX/tdxGetFS.py
@@ -5,45 +5,50 @@ from pytdx.crawler.base_crawler import demo_reporthook
 from pytdx.crawler.history_financial_crawler import HistoryFinancialCrawler
 from  datetime import date
 
+import sys
+mon = date.today().strftime('%m')
+year = date.today().strftime('%Y')
+while mon in ('06','12'):
+    sys.exit(0)
+
 eng = create_engine('postgresql+psycopg2://sa:11111111@10.3.18.56/tdxFS')
 api = TdxHq_API()
 api.connect('119.147.212.81', 7709)
 
-mon = date.today().strftime('%m')
-year = date.today().strftime('%Y')
+
 match mon:
     case mon if mon <= '03':
-        filename='gpcw'+str(int(year)-1)+'1231.zip'
-    case mon if '03'< mon <= '06':
-        filename='gpcw'+year+'0331.zip'
+        ls =['gpcw'+str(int(year)-1)+'1231.zip']
+    case mon if '03'< mon <= '05':
+        ls = ['gpcw'+str(int(year)-1)+'1231.zip', 'gpcw'+year+'0331.zip']
     case mon if '06'< mon <= '09':
-        filename='gpcw'+year+'0630.zip'
-    case mon if '09'< mon <= '12':
-        filename='gpcw'+year+'0930.zip'
-
+        ls =['gpcw'+year+'0630.zip']
+    case mon if '09'< mon < '12':
+        ls =['gpcw'+year+'0930.zip']
+    case mon if mon in ('06','12'):
+        ls = []
 datacrawler = HistoryFinancialCrawler()
 pd.set_option('display.max_columns', None)
 
-
-result = datacrawler.fetch_and_parse(reporthook=demo_reporthook, filename=filename, path_to_download="/tmp/tmpfile.zip")
-dd = datacrawler.to_df(data=result)
-dd['report_date']= dd['report_date'].astype(object)
-upday = dd['report_date'][0]
-dd = dd.round(2)
-dd.to_sql(filename[:-4], eng, if_exists='replace')
-for j,l in enumerate(dd.index.values.tolist()):
-    try:
-        day = pd.read_sql(l, eng)['report_date'].tail(1).tolist()[0]
-        if upday > day:
+for i in ls:
+    result = datacrawler.fetch_and_parse(reporthook=demo_reporthook, filename=i, path_to_download="/tmp/tmpfile.zip")
+    dd = datacrawler.to_df(data=result)
+    dd['report_date']= dd['report_date'].astype(object)
+    upday = dd['report_date'].iloc[0]
+    dd = dd.round(2)
+    dd.to_sql(i[:12],eng,if_exists='replace')
+    for j,l in enumerate(dd.index.values.tolist()):
+        try:
+            day = pd.read_sql(l, eng)['report_date'].tail(1).tolist()[0]
+            if upday > day:
+                pd.DataFrame(dd.iloc[j]).T.reset_index(drop=True).set_index('report_date').to_sql(l, eng, if_exists='append')
+                
+            else:
+                print(l+'not Updated !')
+                pass
+        except:
             pd.DataFrame(dd.iloc[j]).T.reset_index(drop=True).set_index('report_date').to_sql(l, eng, if_exists='append')
-            
-        else:
-            print(l+' : not Updated !')
+            print(l+" =====  New Code add !!")
             pass
-    except:
-        pd.DataFrame(dd.iloc[j]).T.reset_index(drop=True).set_index('report_date').to_sql(l, eng)
-        print(l+" : new to SQL !")
-        pass
 
 eng.dispose()
-
