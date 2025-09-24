0c44eed15a6258b54258728d9e8000a1c789db2e
u588 <u588@sina.com>
2025-09-24 19:40:26 +0800

    v

--- akData/akGetStockIC.py
+++ akData/akGetStockIC.py
@@ -7,6 +7,7 @@ engS = create_engine('postgresql+psycopg://sa:11111111@10.3.18.56/tdxStocks')
 engB = create_engine('postgresql+psycopg://sa:11111111@10.3.18.56/StockBas')
 
 StockLists=pd.read_sql('akStocksList', engS)['StockCode'].to_list()
+# StockLists = pd.read_excel('G:/Gitee/App/TDXapp/tdxAppData/akStocksList.xlsx', dtype={'StockCode':object})['StockCode'].to_list()
 
 df = pd.DataFrame(columns=['新证券简称', '行业中类', '行业大类', '行业次类', '行业门类', '机构名称', '行业编码', '分类标准','分类标准编码', '证券代码', '变更日期'], dtype=object)
 for i in tqdm(StockLists):
@@ -18,6 +19,8 @@ for i in tqdm(StockLists):
 ddf = df[['新证券简称','证券代码','分类标准', '变更日期','行业中类', '行业大类', '行业次类', '行业门类', '行业编码',
     '分类标准编码' ]].rename(columns={'新证券简称':'StockName','证券代码':'StockCode','分类标准':'ICS', '变更日期':'DP','行业中类':'IC4', '行业大类':'IC3', '行业次类':'IC2', '行业门类':'IC1', '行业编码':'ICode',
     '分类标准编码':'ICSCode' })
+
+# ddf.drop_duplicates(subset=(['StockCode','ICS']),keep='last').set_index('StockCode').to_excel('G:/Gitee/App/akData/akStockIC.xlsx')
 ddf.drop_duplicates(subset=(['StockCode','ICS']),keep='last').set_index('StockCode').to_sql('akStockIC', engB, if_exists='replace')
 
 engB.dispose()
